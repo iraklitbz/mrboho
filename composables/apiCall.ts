41f6238cacbbc42c2bdf559b5e39812c33067621
irakli <iraklitbz@gmail.com>
2024-12-23 14:41:49 +0100

    changed the api call

--- composables/apiCall.ts
+++ composables/apiCall.ts
@@ -1,34 +1,26 @@
-
-export async function apiCallSsr (query: any, returnObject?: string, variables: object = {}, cache: boolean = true) {
-    const { data } = await useAsyncQuery({
+export async function apiCall (query: any, returnObject?: string, variables: object = {}, cache: boolean = true, isAuth: boolean = false) {
+    // @ts-expect-error No overload matches this call.
+    const { data }: object = await useAsyncQuery({
         query,
         variables,
-        cache
+        cache,
     })
     if (data?.value && returnObject) {
-        // @ts-expect-error value is not a property of data
         return data?.value[returnObject]
     }
     return data?.value
 }
 
-export async function apiCallClient (query: any, returnObject?: string, variables: object = {}, cache: boolean = true) {
+export async function apiCallClient (query: any, returnObject?: string, variables: object = {}, cache: boolean = true, isAuth: boolean = false) {
     const { resolveClient } = useApolloClient()
     const client = resolveClient()
     const { data } = await client.query({
         query,
         variables,
-        fetchPolicy: cache ? 'cache-first' : 'no-cache'
+        fetchPolicy: cache ? 'cache-first' : 'no-cache',
     })
     if (data && returnObject) {
         return data[returnObject]
     }
     return data?.value
-}
-
-export async function apiCall (query: any, returnObject?: string, variables: object = {}, cache: boolean = true) {
-    if (import.meta.server) {
-        return await apiCallSsr(query, returnObject, variables, cache)
-    }
-    return await apiCallClient(query, returnObject, variables, cache)
 }
