3067d23f7a602edb2b302e1791887cef3365b7c4
irakli <iraklitbz@gmail.com>
2025-03-27 15:02:19 +0100

    added vercel check

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
       script: [
         {
           hid: 'gtmHead',
-          innerHTML: process.env.NODE_ENV === 'production'
+          innerHTML: (process.env.NODE_ENV === 'production' || process.env.VERCEL_ENV === 'production')
               ? `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
