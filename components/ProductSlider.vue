c333e437662ad4ae2d0b25bf1ab3d17d8823f238
gabrieli <g.xokerashvili122018@gmail.com>
2025-12-13 13:44:23 +0100

    წინა მხარის ცვილება

---
+++ components/ProductSlider.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { SunglassesContenfull } from '~/types/contenfull-types';
+
+const props = defineProps({
+  module: {
+    type: Object
+  }
+})
+</script>
+
+<template>
+    <section class="relative mb-10">
+      <div class="sunglasses-grid grid grid-cols-1 gap-5 px-5 md:grid-cols-3 lg:grid-cols-5">
+       
+        <Card
+            v-for="item in module?.productCollection.items"
+            :key="item.id"
+            :product="item as SunglassesContenfull"
+            class="card bg-gray-50"
+            :slug="`${item.__typename === 'Sunglasses' ? 'sunglasses' : 'optical'}/${item.familie?.slug}/${item.slug}`"
+        />
+      </div>
+    </section>
+</template>
+
+<style>
+.no-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+</style>
